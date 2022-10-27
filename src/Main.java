166c8782af3e98a67cbba1b343feec8d0bf6d604
Engel62 <sr.engel@bk.ru>
2022-10-27 21:08:13 +0300

    1 - 2 задание выпонено

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,35 @@ public class Main {
         int age = 12;
         if (age == 18) {
             System.out.println(" С окончанием школы!");
-        } else if (age == 21){
+        } else if (age == 21) {
             System.out.println(" Теперь ты можешь пить! ");
-        } else if (age == 7){
+        } else if (age == 7) {
             System.out.println("Иди в школу!");
         } else {
             System.out.println(" Пока сказат  нечего ");
-    }
+        }
+        // Задание 1
+        System.out.println(" Задание 1");
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1)
+            System.out.println("Установите версию приложения для Android по ссылке");
+
+        // Задание 2
+        System.out.println(" Задание 2");
+
+        int clientDiveceYear = 2015;
+        if (clientOS == 0 && clientDiveceYear >= 2015)
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        else if (clientOS == 0 && clientDiveceYear < 2015)
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        if (clientOS == 1 && clientDiveceYear >= 2015)
+            System.out.println("Установите версию приложения для Android по ссылке");
+        else if (clientOS == 1 && clientDiveceYear < 2015)
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+
+
+
     }
 }
