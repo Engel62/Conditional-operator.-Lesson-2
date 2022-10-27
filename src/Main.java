33a2055755d589536712c70ae3c9efb2de90aba0
Engel62 <sr.engel@bk.ru>
2022-10-27 23:17:22 +0300

    5 задание выпонено

--- src/Main.java
+++ src/Main.java
@@ -51,5 +51,34 @@ public class Main {
         else if (deliveryDistance > 100)
             System.out.println("Доставка не осуществляется");
 
+        // Задание 5
+        System.out.println(" Задание 5");
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
+
+        }
+
     }
 }
